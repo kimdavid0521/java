85f8cd42c6a83127f02b48201fe6ffbe428566e2
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2024-04-16 12:40:41 +0900

    ch1

---
+++ javaStudy/src/CommentJava.java
@@ -0,0 +1,7 @@
+public class CommentJava {
+
+    //psvm치면 자동으로 메서드 나옴
+    public static void main(String[] args) {
+        System.out.println("hello java1"); //sout치면 자동으로 생성됨.
+    }
+}
---
+++ javaStudy/src/variable/Var1.java
@@ -0,0 +1,19 @@
+package variable;
+
+public class Var1 {
+    public static void main(String[] args) {
+        int a,b;
+        a = 20;
+        b=20;
+        int c=20;
+        char ad = 'a';
+        java.lang.String cja = "djsnd";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a+b);
+        System.out.println(c);
+        System.out.println(ad);
+        System.out.println(cja);
+    }
+}
